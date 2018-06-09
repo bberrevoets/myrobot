7589e107cdcab17f59934673d199705becf00521
B.N.Berrevoets <bert@berrevoets.net>
2018-06-09 10:47:05 +0200

    To Cpp

---
+++ src/tf_broadcaster_camera.cpp
@@ -0,0 +1,24 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+
+int main( int argc, char* argv[] )
+{
+    ros::init( argc, argv, "camera_broadcaster" );
+    ros::NodeHandle nh;
+
+    tf::TransformBroadcaster br;
+    tf::Transform transform;
+
+    ros::Rate rate( 10.0 );
+
+    while ( nh.ok() )
+    {
+        transform.setOrigin( tf::Vector3( 0.05, 0.0, 0.15 ) );
+        transform.setRotation( tf::Quaternion( 0, 0, 0, 1 ) );
+        br.sendTransform( tf::StampedTransform( transform, ros::Time::now(), "raspicam", "base_link" ) );
+
+        rate.sleep();
+    }
+
+    return 0;
+}
